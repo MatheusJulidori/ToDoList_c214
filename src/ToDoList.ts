aabb3c58e725dc528507f8d2d998c42a328ae8d0
Matheus Julidori <matheus_julidori@yahoo.com.br>
2024-03-01 17:11:24 -0300

    :sparkles: feat: add tasks to TodoList

---
+++ src/ToDoList.ts
@@ -0,0 +1,16 @@
+export type Task = {
+    title: String,
+    description: String,
+    targetDate: String,
+    type: String,
+    priority: String,
+    subtasks: Task[],
+}
+
+export class ToDoList {
+    private tasks: Task[] = [];
+
+    add(task: Task){
+        this.tasks.push(task);
+    }
+}
